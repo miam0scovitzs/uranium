e30f31c95dc4a37b13da037da84dccf76c0c4e06
unknown <Dibakar Dey>
2022-04-07 23:09:57 +0530

    hii

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,15 +3,30 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+// router.get('/test-me', function (req, res) {
+//     console.log('------------------')
+//     console.log(req)
+//     console.log('------------------')
+//     console.log('These are the request query parameters: ', req.query)
+//     res.send('My first ever api!')
+// });
+// router.get('/candidates', function (req, res) {
+  // console.log('------------------')
+  // console.log(req)
+  // console.log('------------------')
+  // console.log('These are the request query parameters: ', req.query)
+  // res.send('My first ever api!')
 });
 
-
+const arr =[1,2,3,4,5,6,7,8,9];
+let arr1=[];
+router.get('/candidates', function (req, res) {
+  if(req.query.count>0 && req.query.count<=10)
+for(i=0;i<=arr.length;i++){
+  arr1.push(arr[i])
+    } return res.send(arr1)
+  }
+);
 
 
 module.exports = router;
